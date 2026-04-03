29c9bd01b0bfbd6ef5ee90899284a3e042f1a501
softgenai <admin@softgen.ai>
2026-04-03 08:34:30 +0000

    chore(ui): update expired position text description

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: rlsaxidgmokuslxkfdlr
-// Last updated: 2026-04-03T00:03:05.132864
+// Last updated: 2026-04-03T08:30:36.437776
