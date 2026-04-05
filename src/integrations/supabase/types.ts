fcf0a993bbfe9591d0656fd0233479b414778025
softgenai <admin@softgen.ai>
2026-04-05 07:01:22 +0000

    chore(payout): update confirmation text

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: rlsaxidgmokuslxkfdlr
-// Last updated: 2026-04-04T13:04:11.482132
+// Last updated: 2026-04-05T06:55:53.504155
